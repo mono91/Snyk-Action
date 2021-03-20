097cf42d3db5698998073ecba426806e3090e6a9
Jhonatan Marin <jhonatan.marin@ingenian.com>
2021-03-19 21:16:48 -0500

    first commit

---
+++ src/main/java/com/example/snyk/SnykApplication.java
@@ -0,0 +1,13 @@
+package com.example.snyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snyk/SnykApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
